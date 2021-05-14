ed2ef6f0a9b4af369faf58116a96e237fb689fd6
Marion <marion@marion.ee>
2021-05-14 10:10:05 +0300

    11.mai

--- src/views/Meist.vue
+++ src/views/Meist.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="text-center">
-    <h1>Autopesu projektiga on seotud Vali-IT 20 lennu tudengid:</h1>
+    <br>
+    <h1>Autopesu projekti teostasid Vali-IT 20 lennu tudengid:</h1>
     <br>
     <h2>Märt Raudvee</h2>
     <h2>Alari Kukk</h2>
     <h2>Marion Raudsepp</h2>
     <br>
     <h2>juhendaja: Siim Rebane</h2>
+    <br>
+    <h4>mai-2021</h4>
   </div>
 </template>
 
